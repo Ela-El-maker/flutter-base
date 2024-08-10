5813b3ee11cc4457fea96c01f0813e0c87d25b88
Ela-El-maker <feloela@gmail.com>
2024-08-11 01:35:35 +0300

    Runing Dart fix cmd

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -7,7 +7,6 @@ import 'package:initial/presentation/resources/color_manager.dart';
 import 'package:initial/presentation/resources/routes_manager.dart';
 import 'package:initial/presentation/resources/strings_manager.dart';
 import 'package:initial/presentation/resources/values_manager.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
